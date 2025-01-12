3e160aa70a705ec4a637e73ce7d3384a838b06a2
OIsmail99 <osama.m.esmael999@gmail.com>
2025-01-12 08:20:50 +0200

    Time: 0 ms (100%), Space: 41 MB (42.58%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n == 1) return true;
+        if(n <= 0) return false;
+        while(true){
+            if(n % 4 == 0){
+                n = n / 4;
+                if(n==1) return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+}

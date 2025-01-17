f5b8325d91354deda16112b49c010b37a70c1832
OIsmail99 <osama.m.esmael999@gmail.com>
2025-01-17 19:50:38 +0200

    Time: 10 ms (42.7%), Space: 45.6 MB (14.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,9 +1,14 @@
 class Solution { 
     
     public static boolean isValidChar(char c){
-        char[] arr = "abcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
-        for(int i = 0; i < arr.length; i++){
-            if(c == arr[i]) return true;
+        char[] arr = "0123456789abcdefghijklmnopqrstuvwxyz".toCharArray();
+        int low = 0;
+        int high = arr.length - 1;
+        while(low <= high){
+            int mid = (high + low) / 2;
+            if(c == arr[mid]) return true;
+            if(c > arr[mid]) low = mid + 1;
+            if(c < arr[mid]) high = mid - 1;
         }
         return false;
     }

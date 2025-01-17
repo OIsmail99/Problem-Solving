ab02415019bf6d18cb2c7c0a3d7c0b2a6b24a04b
OIsmail99 <osama.m.esmael999@gmail.com>
2025-01-17 19:42:23 +0200

    Time: 10 ms (42.7%), Space: 45.9 MB (11.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,22 @@
 class Solution { 
+    
+    public static boolean isValidChar(char c){
+        char[] arr = "abcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+        for(int i = 0; i < arr.length; i++){
+            if(c == arr[i]) return true;
+        }
+        return false;
+    }
+
     public boolean isPalindrome(String s) {
         String str = s.toLowerCase();
         int start = 0;
         int end = str.length() - 1;
         while(start < end){
-            while(start < end && !Character.isLetterOrDigit(str.charAt(start))){
+            while(start < end && !isValidChar(str.charAt(start))){
                 start++;
             }
-        while(start < end && !Character.isLetterOrDigit(str.charAt(end))){
+        while(start < end && !isValidChar(str.charAt(end))){
                 end--;
             }
             if(str.charAt(start) != str.charAt(end)){

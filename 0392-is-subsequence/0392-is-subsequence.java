afa0f501af3e30a1ef844045300a640085bc364f
OIsmail99 <osama.m.esmael999@gmail.com>
2025-01-22 07:42:31 +0200

    Time: 1 ms (93.37%), Space: 41.4 MB (83.84%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,21 +1,20 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-        if(s.equals(t)) return true;
-        if(s.length() > t.length()) return false;
-        int s_index = s.length() -1;
-        int t_index = t.length() -1;
-        if(s.length() ==0)return true;
-        while(t_index >= 0){ //abc //ahbgdc
-            if(s.charAt(s_index) == t.charAt(t_index)){
-                s_index--;
-                t_index--;
-                if(s_index == -1) return true;
-            }
-            else{
-                t_index--;
-            }
+   public boolean isSubsequence(String s, String t) {
+    if (s.length() == 0) return true; // An empty string is always a subsequence
+    if (s.length() > t.length()) return false; // `s` cannot be a subsequence of a shorter string
+
+    int s_index = 0; // Start from the beginning of `s`
+    int t_index = 0; // Start from the beginning of `t`
+
+    while (t_index < t.length()) {
+        if (s.charAt(s_index) == t.charAt(t_index)) {
+            s_index++; // Move to the next character in `s`
+            if (s_index == s.length()) return true; // to avoid outOfBounds
         }
-        if(s_index >=0) return false;
-        return true;
+        t_index++; // Always move to the next character in `t`
     }
+
+    return false; // If loop finishes and `s_index` < `s.length()`, `s` is not a subsequence
+}
+
 }

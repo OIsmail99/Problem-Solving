a896f74d27e93202f25e857f70433ff5790dc085
OIsmail99 <osama.m.esmael999@gmail.com>
2025-01-25 19:15:45 +0200

    Time: 1 ms (65.33%), Space: 44.8 MB (76.52%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,12 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        // uniqueIndex advances with each unique element added to the beginning.
+        // uniqueVal: the first element in a series of duplicates.
         int uniqueIndex = 0;
-        int currentVal = nums[0]; 
+        int uniqueVal = nums[0]; 
         for(int i=1; i < nums.length; i++){
-            if(nums[i] != currentVal){
-                currentVal = nums[i];
+            if(nums[i] != uniqueVal){
+                uniqueVal = nums[i];
                 uniqueIndex++;
-                nums[uniqueIndex] = currentVal;
+                nums[uniqueIndex] = uniqueVal;
             }
         }
         return uniqueIndex+1;

9b172070e68cda55e49e8fbfbfb6ec896aaac119
OIsmail99 <osama.m.esmael999@gmail.com>
2025-01-26 18:08:42 +0200

    Time: 1946 ms (20.75%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -3,6 +3,6 @@
 SELECT s.student_id, s.student_name, sub.subject_name, count(e.subject_name) as attended_exams
 FROM Students s join Subjects sub
 left join examinations e on s.student_id = e.student_id AND sub.subject_name = e.subject_name
-group by 1,2,3
+group by 1,3
 order by 1,3;
 

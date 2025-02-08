de3d868bba792b9e0973a6e45d8f791d2d715684
OIsmail99 <osama.m.esmael999@gmail.com>
2025-02-08 02:04:24 +0200

    Time: 18 ms (82.15%), Space: 58.2 MB (28.6%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -5,18 +5,19 @@ class Solution {
         if(map.containsKey(nums[i])){ //i=2
             //[1,0,1,1]
             int j = map.get(nums[i]); //j=9
-            if(Math.abs(i - j) <= k){
+            if(i - j <= k){
                 return true;
                 }
-            else{
-                map.put(nums[i], i);
-            }
+            // else{
+            //     map.put(nums[i], i);
+            // }
 
-        }
-        else{
-            map.put(nums[i], i);//[1:0,0:1]
-        }
+        // }
+        // else
        }
+            map.put(nums[i], i);//[1:0,0:1]
+       }
+        
        return false;
     }
 }

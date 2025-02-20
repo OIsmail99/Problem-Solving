4b7554924aa5f482514ec1e44022abba7fbf09d7
OIsmail99 <osama.m.esmael999@gmail.com>
2025-02-20 08:55:42 +0200

    Time: 55 ms (92.98%), Space: 55.1 MB (5.53%) - LeetHub

--- 2741-function-composition/2741-function-composition.js
+++ 2741-function-composition/2741-function-composition.js
@@ -3,14 +3,9 @@
  * @return {Function}
  */
 var compose = function(functions) {
-
+    
     return function(x) {
-        if(functions.length === 0) return x;
-        let res = functions[functions.length-1](x);
-        for(let i=functions.length -2; i >=0 ; i--){
-             res = functions[i](res)
-        }
-        return res    
+        return functions.reduceRight((acc, func) => func(acc) , x);
     }
 };
 

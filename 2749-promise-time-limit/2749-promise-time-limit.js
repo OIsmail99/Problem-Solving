00736eb121e2115b264c28e163c37bcb65736fc3
OIsmail99 <osama.m.esmael999@gmail.com>
2025-02-26 07:08:54 +0200

    Time: 51 ms (87.52%), Space: 55 MB (5.58%) - LeetHub

--- 2749-promise-time-limit/2749-promise-time-limit.js
+++ 2749-promise-time-limit/2749-promise-time-limit.js
@@ -8,11 +8,12 @@ var timeLimit = function(fn, t) {
     return async function(...args) {
 
         return new Promise((resolve, reject) => {
-           setTimeout(()=> reject("Time Limit Exceeded"), t);
+          const id = setTimeout(()=> reject("Time Limit Exceeded"), t);
            
            fn(...args)
            .then((res) => resolve(res))
-           .catch((err) => reject(err));
+           .catch((err) => reject(err))
+           .finally(() => clearTimeout(id));
         })
     }
 };

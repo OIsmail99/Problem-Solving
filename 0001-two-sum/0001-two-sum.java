d38749bcf76ffbe36e4ff113e240a8c70239fe58
Osama Ismail <osama.m.esmael999@gmail.com>
2025-05-18 23:34:36 +0300

    Time: 3 ms (61.09%), Space: 44.9 MB (77.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>(); //number and its index
-        
-        
+        Map<Integer, Integer> map = new HashMap<>();
         for(int i=0; i < nums.length; i++){
-                int complement = target - nums[i];
-
-                if(map.containsKey(complement)){
-                    return new int[] {map.get(complement), i};
+            int complement = target - nums[i];
+            if(map.containsKey(complement)){
+                int[] arr = {map.get(complement), i};
+                return arr;
             }
             else{
-                map.put(nums[i], i);
+            map.put(nums[i], i);
             }
         }
-        throw new IllegalArgumentException("not found"); 
+        return null;
     }
 }

1384301e034b1467a80943354deea4f370d4e27b
Osama Ismail <osama.m.esmael999@gmail.com>
2025-06-16 22:48:47 +0300

    Time: 891 ms (77.23%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -1,8 +1,17 @@
 # Write your MySQL query statement below
-#find the number of times each student attended each exam.
-SELECT s.student_id, s.student_name, sub.subject_name, count(e.subject_name) as attended_exams
-FROM Students s join Subjects sub
-left join examinations e on s.student_id = e.student_id AND sub.subject_name = e.subject_name
-group by 1,3
+-- Write a solution to find the number of times each student attended each exam.
+-- Return the result table ordered by student_id and subject_name.
+SELECT s.student_id, s.student_name, sub.subject_name, count(e.student_id) as attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e
+ON s.student_id = e.student_id and sub.subject_name = e.subject_name
+GROUP BY 1,2,3
 order by 1,3;
+
+
+
+
+
+
 
